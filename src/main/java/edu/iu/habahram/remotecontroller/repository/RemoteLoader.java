ea2b3a487caf296db16d46801b20f42fd9f08989
millemaliu <millemal@iu.edu>
2024-02-26 08:52:31 -0500

    init

---
+++ src/main/java/edu/iu/habahram/remotecontroller/repository/RemoteLoader.java
@@ -0,0 +1,47 @@
+package edu.iu.habahram.remotecontroller.repository;
+
+import edu.iu.habahram.remotecontroller.model.DeviceData;
+import edu.iu.habahram.remotecontroller.model.Light;
+import edu.iu.habahram.remotecontroller.model.RemoteControl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class RemoteLoader implements  IRemoteLoader{
+
+    public static RemoteLoader uniqueInstance = new RemoteLoader();
+    HashMap<Integer, RemoteControl> remoteControls = new HashMap<>();
+    @Override
+    public void setup(int id, List<DeviceData> devices) {
+        RemoteControl remoteControl = new RemoteControl(devices.size());
+        for(DeviceData device : devices) {
+            switch (device.type()) {
+                case "light":
+                    Light light = new Light(device.location());
+                    remoteControl.setCommand(device.slot(), light::on, light::off);
+                    break;
+            }
+        }
+        remoteControls.put(id, remoteControl);
+        System.out.println(remoteControl.toString());
+    }
+    
+    private RemoteLoader() {}
+    
+    
+    public static RemoteLoader getInstance() {
+        return uniqueInstance
+    }
+
+    @Override
+    public String onButtonWasPushed(int id, int slot) {
+         return remoteControls.get(id).onButtonWasPushed(slot);
+    }
+
+    @Override
+    public String offButtonWasPushed(int id, int slot) {
+        return remoteControls.get(id).offButtonWasPushed(slot);
+
+    }
+}
